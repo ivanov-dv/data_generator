919f7e281eac025cd4e262a352b9acb84eca4e4b
Denis Ivanov <dnb8866@gmail.com>
2024-09-10 16:20:14 +0700

    Add docstrings.

--- utils/models.py
+++ utils/models.py
@@ -2,18 +2,24 @@ import enum
 
 
 class FileFormat(enum.Enum):
+    """Enum class for file formats. Values are the user's choice in the terminal"""
     XLSX = '1'
     CSV = '2'
     TXT = '3'
 
 
 class PackerFormat(enum.Enum):
+    """Enum class for packer formats. Values are the user's choice in the terminal'"""
     ZIP = '1'
     FORMAT_7Z = '2'
     NO_PACKER = '3'
 
     @classmethod
-    def field_name(cls, value):
+    def field_name(cls, value) -> str:
+        """
+        Create description for user input.
+        :param value: User input.
+        """
         field_names = {
             '1': 'zip',
             '2': '7z (поддерживает разбиение на части).',
@@ -23,11 +29,16 @@ class PackerFormat(enum.Enum):
 
 
 class PackerType(enum.Enum):
+    """Enum class for packer types. Values are the user's choice in the terminal'"""
     ONE_FILE = '1'
     PART_FILES = '2'
 
     @classmethod
-    def field_name(cls, value):
+    def field_name(cls, value) -> str:
+        """
+        Create description for user input.
+        :param value: User input.
+        """
         field_names = {
             '1': 'Архивировать в один файл',
             '2': 'Указать максимальный размер архива.'
@@ -36,11 +47,16 @@ class PackerType(enum.Enum):
 
 
 class WorkFormat(enum.Enum):
+    """Enum class for work formats. Values are the user's choice in the terminal'"""
     GENERATOR = '1'
     PACKER = '2'
 
     @classmethod
-    def field_name(cls, value):
+    def field_name(cls, value) -> str:
+        """
+        Create description for user input.
+        :param value: User input.
+        """
         field_names = {
             '1': 'Сгенерировать случайные данные в файл или архив',
             '2': 'Заархивировать файл или файлы.'
